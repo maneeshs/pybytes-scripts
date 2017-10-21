0c096368e89fc8200b4d1cc456505d6d73d1c73c
jirikrepl <jiri.krepl@gmail.com>
2017-10-21 20:07:59 +0200

    Added bash script for starting Pybytes microservices

---
+++ start-pybytes.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# to get this script working, install ttab before
+# npm install ttab -g
+
+cat << "EOF"
+
+    ____                                ____        __          __
+   / __ \__  ___________  ____ ___     / __ \__  __/ /_  __  __/ /____  _____
+  / /_/ / / / / ___/ __ \/ __ `__ \   / /_/ / / / / __ \/ / / / __/ _ \/ ___/
+ / ____/ /_/ / /__/ /_/ / / / / / /  / ____/ /_/ / /_/ / /_/ / /_/  __(__  )
+/_/    \__, /\___/\____/_/ /_/ /_/  /_/    \__, /_.___/\__, /\__/\___/____/
+      /____/                              /____/      /____/
+
+EOF
+
+PYBYTES_DIR='.'
+cd ${PYBYTES_DIR}
+
+read -r -p "Start mongod? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+
+    ps cax | grep mongod > /dev/null
+    if [ $? -eq 0 ]; then
+      killall mongod
+    fi
+
+    ttab -t mongod -G mongod
+fi
+
+read -r -p "Start redis-server? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+
+    ps cax | grep redis-server > /dev/null
+    if [ $? -eq 0 ]; then
+      redis-cli shutdown
+    fi
+
+    ttab -t redis-server -G redis-server
+fi
+
+read -r -p "Start mqtt-server? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+    ttab -t mqtt-server -G npm run --prefix mqttserver develop
+fi
+
+read -r -p "Start PyBill? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+    ttab -t PyBill -G npm run --prefix pybill develop
+fi
+
+read -r -p "Start PyAuth? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+    ttab -t PyAuth -G npm run --prefix pyauth develop
+fi
+
+read -r -p "Start Pybytes-api? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+    ttab -t Pybytes-api -G npm run --prefix pybytes-api develop
+fi
+
+read -r -p "Start Pybytes-react? [Y/n] " response
+if [[ ${response} =~ ^(yes|Y|y| ) ]] || [ -z ${response} ]; then
+    ttab -t Pybytes-react -G npm run --prefix pybytes-react develop
+fi
